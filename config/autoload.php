13c6547a783e643277bc3ae1adcc78089d52bd20
Christof W. Raidler <c.raidler@rad-consulting.ch>
2017-02-24 16:51:38 +0100

    Initial

---
+++ config/autoload.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Contao extension for RAD Consulting GmbH
+ *
+ * @copyright  RAD Consulting GmbH 2016
+ * @author     Chris Raidler <c.raidler@rad-consulting.ch>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+
+// Log
+\NamespaceClassLoader::add('RAD', __DIR__ . '/../library');
