c341d0247af48f29031db841c41a10b20c500a79
Christof W. Raidler <c.raidler@rad-consulting.ch>
2017-02-25 13:20:09 +0100

    Initial

---
+++ library/RAD/Log/LogException.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @copyright  RAD Consulting GmbH 2017
+ * @author     Chris Raidler <c.raidler@rad-consulting.ch>
+ * @author     Olivier Dahinden <o.dahinden@rad-consulting.ch>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+namespace RAD\Log;
+
+use Exception;
+
+/**
+ * Class LogException
+ */
+class LogException extends Exception
+{
+    /**
+     * @var string
+     */
+    protected $data;
+
+    /**
+     * LogException constructor.
+     *
+     * @param string      $message
+     * @param int         $code
+     * @param Exception   $previous
+     * @param string|null $data
+     */
+    public function __construct($message, $code, Exception $previous, $data = null)
+    {
+        parent::__construct($message, $code, $previous);
+        $this->data = $data;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getData()
+    {
+        return $this->data;
+    }
+}
